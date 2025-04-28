0b3625fa21713fa1793e3863034584dfaa2bd768
PhucPhamXuan <91009245+PhucPhamXuan@users.noreply.github.com>
2025-04-28 23:04:43 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,30 @@
+const CACHE_NAME = 'phuihub-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Khi cài đặt Service Worker
 self.addEventListener('install', function(event) {
-  console.log('Service Worker: Installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Đã mở cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('Service Worker: Activated');
-});
-
+// Khi fetch tài nguyên
 self.addEventListener('fetch', function(event) {
-  // Để trống - tương lai bạn có thể thêm cache
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - trả về cache
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 });
